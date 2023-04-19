19af489ce5b0eb288e977d6e7e2b2e53518ee50b
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-19 20:22:34 +0000

    Format the generated schedule into a table view.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import pandas as pd
 import requests
 import os
 import openai
@@ -32,7 +33,7 @@ def generate_calendar():
     try:
         response = openai.Completion.create(
             engine="text-davinci-002",
-            prompt=f"Create a calendar schedule for the following activities on {event_date}:\n{activities}\n The calendar should include normal everyday activities even if they are not mentioned, like eating breakfast, lunch and dinner. And a normal weekday schedule should begin at 6 am and end at 10 pm PT. A normal weekend schedule should begin at 7 am and end at 11 pm PT",
+            prompt=f"Create a calendar schedule for the following activities on {event_date}:\n{activities}\n The calendar should include normal everyday activities even if they are not mentioned, like eating breakfast, lunch and dinner. It should not include going to work, because I work from home. Prioritize more time spent on work related activities and less time on liesure related activities. And a normal weekday schedule should begin at 6 am and end at 10 pm PT. A normal weekend schedule should begin at 7 am and end at 11 pm PT. Each activity should include the activity name and a start and end time with no overlapping activities.",
             temperature=0.5,
             max_tokens=200,
             top_p=1,
@@ -41,11 +42,32 @@ def generate_calendar():
         )
 
         generated_schedule = response.choices[0].text.strip()
-        print("Generated Schedule:", generated_schedule)  # Log the generated schedule
-    except Exception as e:
-        return jsonify({"error": str(e)})
+      
+        # Split the generated schedule into individual activities
+        activities_list = generated_schedule.split("\n\n")
+        activities_list = [activity.split("\n") for activity in activities_list]
+    
+        # Create a list of dictionaries with activity name, start time, and end time
+        activities_dict_list = []
+        for activity in activities_list:
+            activity_dict = {}
+            for i in range(len(activity)):
+                if i == 0:
+                    activity_dict["name"] = activity[i]
+                elif "start time" in activity[i]:
+                    activity_dict["start_time"] = activity[i].split(": ")[1]
+                elif "end time" in activity[i]:
+                    activity_dict["end_time"] = activity[i].split(": ")[1]
+            activities_dict_list.append(activity_dict)
+    
+        # Create a table view with activity name, start time, and end time
+        table = "<table><tr><th>Activity</th><th>Start Time</th><th>End Time</th></tr>"
+        for activity in activities_dict_list:
+            table += f"<tr><td>{activity['name']}</td><td>{activity['start_time']}</td><td>{activity['end_time']}</td></tr>"
+        table += "</table>"
+    
+        return jsonify({"schedule": table})
 
-    return jsonify({"schedule": generated_schedule})
   
 if __name__ == '__main__':
     app.run(debug=False)
