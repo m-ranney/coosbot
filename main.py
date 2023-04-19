d965b236acea9a4a9133ff182853da0da71a309a
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-19 15:18:02 +0000

    Calendar updates. Added calendar html file and updated main.py with new app route.

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def home():
     return render_template('home.html')
 
 # More routes for other actions
-
+@app.route('/calendar', methods=['GET', 'POST'])
+def calendar():
+    if request.method == 'POST':
+        # Process the calendar details and integrate with ChatGPT API
+        # Then, integrate with Google Calendar
+        pass
+    return render_template('calendar.html')
+  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
