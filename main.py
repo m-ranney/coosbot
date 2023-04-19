fbab6ce269def5ecc7547135a4219b41de145432
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-19 17:06:44 +0000

    Update calendar code.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
+import os
+import openai
 
 app = Flask(__name__)
+
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = openai_api_key
 
 # Define your routes here
 @app.route('/')
@@ -9,13 +14,11 @@ def home():
     return render_template('home.html')
 
 # More routes for other actions
-@app.route('/calendar', methods=['GET', 'POST'])
+@app.route('/calendar')
 def calendar():
-    if request.method == 'POST':
-        # Process the calendar details and integrate with ChatGPT API
-        # Then, integrate with Google Calendar
-        pass
-    return render_template('calendar.html')
+    tomorrow = date.today() + timedelta(days=1)
+    default_date = tomorrow.isoformat()
+    return render_template('calendar.html', default_date=default_date)
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
