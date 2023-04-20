d53d502f9fadd61fd9a3841bbb263c50c2879e79
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-04-20 17:09:09 +0000

    Bug fixes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pytz
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
